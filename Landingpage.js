666d73207931e4effd98ed2541eb55aad7cf7f56
Cassius <casskomp@gmx.de>
2024-01-18 08:45:48 +0100

    small test, svg header

--- Landingpage.js
+++ Landingpage.js
@@ -4,6 +4,7 @@ document.addEventListener('DOMContentLoaded', function() {
     const secondElement = document.getElementById("second2");
     const thirdElement = document.getElementById("third3");
     const fourthElement = document.getElementById("fourth4");
+    const buchenButton = document.getElementById("buchen");
     
     const erwTitle = document.getElementById("erwTitle");
     const firstTitle = document.getElementById("firstTitle");
@@ -13,7 +14,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
     const disappear = document.querySelectorAll(".disappear");
     const backElement = document.getElementById("back1");
-    const buchenElement = document.querySelector(".buchen");
+    const buchenElement = document.getElementById("buchen");
     // const backgroundElement = document.getElementById("background");
     const clickedContainer = document.querySelectorAll(".top-leftklick, .top-rightklick, .middle-leftklick, .middle-rightklick"); 
 
@@ -47,196 +48,6 @@ document.addEventListener('DOMContentLoaded', function() {
 
 
 
-// Step 1: Select the element
-var moreinfo = document.getElementById('moreinfo');
-const circleI = document.getElementById('function_button');
-const circle2 = document.getElementById('arrow_container');
-const info = document.querySelector('.info');
-// Step 2: Create a function that sets the width
-
-function setWidth() {
-  circleI.style.transition = 'transform 0.5s ease-in-out';
-  circleI.style.transform = 'rotate(360deg)';
-  moreinfo.style.width = 'calc(45vw - 0.5rem)';
-  moreinfo.style.opacity = '1'
-  moreinfo.style.zIndex = '900';
-  moreinfo.style.pointerEvents = 'auto';
-// setTimeout(() => {
-// }, 150);
-
-  setTimeout(() => {
-
-    circle2.style.opacity = '1';
-    circle2.style.transition = 'transform 0.65s ease';
-  circle2.style.transform = 'rotate(360deg)';
-            }, 250);
-  setTimeout(() => {
-                info.style.opacity = '0';
-            }, 350);
-}
-
-
-
-// Step 3: Call the function every 5 seconds
-var intervalId = setInterval(setWidth, 4000);
-
-// Step 4: Stop the interval after 5 seconds
-setTimeout(function() {
-  clearInterval(intervalId);
-}, 5000);
-
-    var autoExpandTimeout, autoContractTimeout;
-    var isManuallyToggled = false;
-    var firstExpansionDone = false;
-
-    // Function to toggle the expansion of the content area
-    function toggleContentArea(expand) {
-        var contentArea = document.getElementById('contentArea');
-        contentArea.classList.toggle('expanded', expand);
-//         setTimeout(() => {
-//           moreinfo.style.width = '8rem';
-//         moreinfo.style.opacity = '1'
-//         moreinfo.style.zIndex = '999';
-//         moreinfo.style.pointerEvents = 'auto';
-// },1000);
-
-    }
-
-
-
-
-    /* TEAM JS*/
-
-    // document.querySelector('.team_img_container').addEventListener('mouseover', function() {
-    //     document.querySelector('.team_info_extra').style.display = 'block';
-    // });
-    
-    // document.querySelector('.hover-element').addEventListener('mouseout', function() {
-    //     document.querySelector('.team_info_extra').style.display = 'none';
-    // });
-    
-    //FIX manual toggle
-
-    // Event listener for manual toggle by clicking the circle button
-    document.getElementById('container_expand').addEventListener('click', function() {
-        clearTimeout(autoExpandTimeout);
-        clearTimeout(autoContractTimeout);
-        moreinfo.style.width = '35vw';
-        moreinfo.style.opacity = '1'
-        moreinfo.style.zIndex = '999';
-        moreinfo.style.pointerEvents = 'auto';
-
-
-        var isExpanded = document.getElementById('contentArea').classList.contains('expanded');
-        isManuallyToggled = true; 
-        toggleContentArea(!isExpanded);
-        
-        if (!isExpanded) {
-            autoExpandTimeout = setTimeout(handleAutomaticToggle, 10000);
-            moreinfo.style.width = '3rem';
-        moreinfo.style.opacity = '0'
-        moreinfo.style.zIndex = '0';
-        moreinfo.style.pointerEvents = 'none';
-        }
-    });
-
-    // Function to handle the automatic expansion and contraction
-    function handleAutomaticToggle() {
-        if (!isManuallyToggled) {
-            // Expand the content area
-            toggleContentArea(true);
-            
-            // Contract the content area after 10 seconds
-            autoContractTimeout = setTimeout(function() {
-                toggleContentArea(false);
-                moreinfo.style.width = '3rem';
-                moreinfo.style.opacity = '0';
-                moreinfo.style.zIndex = '0';
-                moreinfo.style.pointerEvents = 'none';
-
-                setTimeout(() => {
-        circle2.style.transform = 'rotate(360deg)'; 
-        circle2.style.transition = 'transform 0.65s ease';
-
-      }, 10);
-
-
-      setTimeout(() => {
-
-        info.style.opacity = '1';
-        info.style.transition = 'transform 0.65s ease';
-        info.style.transform = 'rotate(360deg)';
-                }, 250);
-
-      setTimeout(() => {
-        circle2.style.transform = 'rotate(360deg)'; 
-
-        circle2.style.opacity = '0';
-
-      }, 350);
-
-
-
-                // Set delay for next automatic expansion
-                autoExpandTimeout = setTimeout(handleAutomaticToggle, firstExpansionDone ? 10000 : 3000);
-                firstExpansionDone = true;
-                isManuallyToggled = false; // Reset the manual flag after automatic contraction
-
-            }, 30000);
-        }
-    }
-
-    // Start the automatic toggle process with an initial delay of 3 seconds
-    autoExpandTimeout = setTimeout(handleAutomaticToggle, 3000);
-
-
-
-
-    // var autoExpandTimeout, autoContractTimeout;
-    // var isManuallyToggled = false;
-    // var firstExpansionDone = false;
-
-    // // Function to toggle the expansion of the content area
-    // function toggleContentArea(expand) {
-    //     var contentArea = document.getElementById('contentArea');
-    //     contentArea.classList.toggle('expanded', expand);
-    // }
-
-    // // Event listener for manual toggle by clicking the circle button
-    // document.getElementById('circleButton').addEventListener('click', function() {
-    //     clearTimeout(autoExpandTimeout);
-    //     clearTimeout(autoContractTimeout);
-
-    //     var isExpanded = document.getElementById('contentArea').classList.contains('expanded');
-    //     isManuallyToggled = true; // Set the flag to indicate manual toggle
-    //     toggleContentArea(!isExpanded);
-        
-    //     // Set delay for next automatic expansion after manual contraction
-    //     if (!isExpanded) {
-    //         autoExpandTimeout = setTimeout(handleAutomaticToggle, 10000);
-    //     }
-    // });
-
-    // // Function to handle the automatic expansion and contraction
-    // function handleAutomaticToggle() {
-    //     if (!isManuallyToggled) {
-    //         // Expand the content area
-    //         toggleContentArea(true);
-            
-    //         // Contract the content area after 10 seconds
-    //         autoContractTimeout = setTimeout(function() {
-    //             toggleContentArea(false);
-
-    //             // Set delay for next automatic expansion
-    //             autoExpandTimeout = setTimeout(handleAutomaticToggle, firstExpansionDone ? 10000 : 3000);
-    //             firstExpansionDone = true;
-    //             isManuallyToggled = false; // Reset the manual flag after automatic contraction
-    //         }, 10000);
-    //     }
-    // }
-
-    // // Start the automatic toggle process with an initial delay of 3 seconds
-    // autoExpandTimeout = setTimeout(handleAutomaticToggle, 3000);
 
     erwachsene.addEventListener('click', function() {
         if (linkEnabledFirst) {
@@ -282,7 +93,7 @@ setTimeout(function() {
             erwachsene.style.height = '15%';
             erwTitle.style.opacity= '0';
             // erwInnerCircle.style.height = '100%';
-            
+            buchenButton.style.opacity = '1';
             buchenElement.setAttribute("href", "https://www.swym-hamburg.de/kursuebersicht.html#/Erwachsene%20-%20Schwimmlernkurs%20(Stufe%201)");
             
             
@@ -386,6 +197,7 @@ setTimeout(function() {
             linkEnabledFirst = true;
             firstElement.style.height = '15%';
             firstTitle.style.opacity= '0';
+            buchenButton.style.opacity = '1';
 
             buchenElement.setAttribute("href", "https://www.swym-hamburg.de/kursuebersicht.html#/Kinder%20-%20Schwimmlernkurse%2f-vorschule%20(Stufe%201)");
             // Passive circles
@@ -485,6 +297,7 @@ setTimeout(function() {
             header.style.backgroundColor = 'rgb(0, 125, 195)';
             linkEnabledSecond = true;
             secondTitle.style.opacity= '0';
+            buchenButton.style.opacity = '1';
 
             buchenElement.setAttribute("href", "https://www.swym-hamburg.de/kursuebersicht.html#/Kinder%20-%20Schwimmlernkurse%2f-vorschule%20(Stufe%201)");
 
@@ -580,6 +393,7 @@ setTimeout(function() {
             linkEnabledThird = true;
             thirdTitle.style.opacity= '0';
             buchenElement.setAttribute("href", "https://www.swym-hamburg.de/kursuebersicht.html#/Kinder%20-%20Seepferdchenkurs%20(Stufe%202)");
+            buchenButton.style.opacity = '1';
 
             //PASSIVE CIRCLES
             erwachsene.style.pointerEvents = 'none';
@@ -673,6 +487,7 @@ setTimeout(function() {
             linkEnabledFourth = true;           
             fourthTitle.style.opacity= '0';
             buchenElement.setAttribute("href", "https://www.swym-hamburg.de/kursuebersicht.html#/Kinder%20-%20Schwimmclub%20(Stufe%203)");
+            buchenButton.style.opacity = '1';
 
             // passive circles
             erwachsene.style.opacity = '0';
@@ -793,6 +608,7 @@ setTimeout(function() {
             secondInnerCircle.style.height = '';
             thirdInnerCircle.style.height = '';
             fourthInnerCircle.style.height = '';
+            buchenButton.style.opacity = '';
 
             //erwachsene
             erwachsene.style.height = '';
